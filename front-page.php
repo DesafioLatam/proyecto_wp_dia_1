8bd56279d921c97a83b942615d54c1104f7ea94b
Gonzalo Salinas <gsalinase91@gmail.com>
2018-02-12 18:16:21 -0300

    first commit

---
+++ front-page.php
@@ -0,0 +1,220 @@
+<?php get_header() ?>
+
+<!-- Hero Section -->
+   <header class="masthead text-center text-white d-flex">
+     <div class="container my-auto">
+       <div class="row">
+         <div class="col-lg-10 mx-auto">
+           <h1 class="text-uppercase">
+             <strong>Tu marca, es tu verdadera identidad</strong>
+           </h1>
+           <hr>
+         </div>
+         <div class="col-lg-8 mx-auto">
+           <p class="text-faded mb-5">Consultorias en cambios de marcas para empresas</p>
+           <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Descubre más acá</a>
+         </div>
+       </div>
+     </div>
+   </header>
+
+   <!-- About Section -->
+   <section class="bg-primary" id="about">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-8 mx-auto text-center">
+           <h2 class="section-heading text-white">Nos dedicamos a potenciar tu marca</h2>
+           <hr class="light my-4">
+           <p class="text-faded mb-4">Desde hace más de 30 años, estamos junto a las empresas ayudándolas a definir su identidad.</p>
+           <button class="btn btn-light btn-xl text-center"><a class="js-scroll-trigger" href="#services">Nuestros servicios</a></button>
+         </div>
+       </div>
+     </div>
+   </section>
+
+   <!-- Services Section -->
+   <section id="services">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-12 text-center">
+           <h2 class="section-heading">Nuestros servicios</h2>
+           <hr class="my-4">
+         </div>
+       </div>
+     </div>
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-3 col-md-6 text-center">
+           <div class="service-box mt-5 mx-auto">
+             <i class="fa fa-4x fa-diamond text-primary mb-3 sr-icons"></i>
+             <h3 class="mb-3">Branding y Consultoria</h3>
+             <p class="text-muted mb-0">Desde la identidad visual hasta las garantías, ¿transmite la voz y la personalidad de su empresa de manera coherente.</p>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 text-center">
+           <div class="service-box mt-5 mx-auto">
+             <i class="fa fa-4x fa-paper-plane text-primary mb-3 sr-icons"></i>
+             <h3 class="mb-3">Creatividad y Desarrollo de Marca</h3>
+             <p class="text-muted mb-0">Se puede usar un buen diseño, pero vamos más allá: Útil, accesible y diseñado teniendo en cuenta la filosofía de la marca.</p>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 text-center">
+           <div class="service-box mt-5 mx-auto">
+             <i class="fa fa-4x fa-newspaper-o text-primary mb-3 sr-icons"></i>
+             <h3 class="mb-3">Marketing Digital</h3>
+             <p class="text-muted mb-0">Un plan de marketing integrado, que se crea teniendo en cuenta el panorama general de tu negocio.</p>
+           </div>
+         </div>
+         <div class="col-lg-3 col-md-6 text-center">
+           <div class="service-box mt-5 mx-auto">
+             <i class="fa fa-4x fa-heart text-primary mb-3 sr-icons"></i>
+             <h3 class="mb-3">Contenido y Medios Sociales</h3>
+             <p class="text-muted mb-0">Juega según sus reglas, teniendo una gran cantidad de contenido que atraiga a tus clientes hacia ti en su búsqueda.
+             </p>
+           </div>
+         </div>
+       </div>
+     </div>
+   </section>
+
+   <!-- Portfolio Section -->
+   <section class="p-0" id="portfolio">
+     <div class="container-fluid p-0">
+       <h2 class="section-heading text-center p-2">Últimos trabajos</h2>
+       <hr class="my-4">
+       <div class="row no-gutters popup-gallery">
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/1.jpg">
+             <img class="img-fluid" src="img/portfolio/1.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Vin-age.cl
+                 </div>
+                 <div class="project-name">
+                   Sesión fotográfica y mejora de marca par vin.age.cl
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/2.jpg">
+             <img class="img-fluid" src="img/portfolio/2.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Chapitas.cl
+                 </div>
+                 <div class="project-name">
+                 Mejora de imagen a tienda de regalos corporativos.
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/3.jpg">
+             <img class="img-fluid" src="img/portfolio/3.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Cámaras Don Pepito
+                 </div>
+                 <div class="project-name">
+                   Mejora de marca y sesión fotográfica a local.
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/4.jpg">
+             <img class="img-fluid" src="img/portfolio/4.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Di Tutto Restaurant
+                 </div>
+                 <div class="project-name">
+                   Mejora de marca en el restaurant Di Tutto.
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/5.jpg">
+             <img class="img-fluid" src="img/portfolio/5.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Costuras Juanita
+                 </div>
+                 <div class="project-name">
+                     Mejora visual de la tienda y sesión fotográfica.
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+         <div class="col-lg-4 col-sm-6">
+           <a class="portfolio-box" href="img/portfolio/6.jpg">
+             <img class="img-fluid" src="img/portfolio/6.jpg" alt="">
+             <div class="portfolio-box-caption">
+               <div class="portfolio-box-caption-content">
+                 <div class="project-category text-faded">
+                   Herrerias Herrera
+                 </div>
+                 <div class="project-name">
+                   Sesión fotográfica y puesta en marcha de servicios en redes sociales.
+                 </div>
+               </div>
+             </div>
+           </a>
+         </div>
+       </div>
+     </div>
+   </section>
+
+   <!-- Contact section -->
+   <section id="contact">
+     <div class="container">
+       <div class="row">
+         <div class="col-lg-8 mx-auto text-center">
+           <h2 class="section-heading">Contáctanos</h2>
+           <hr class="my-4">
+           <p class="mb-5">¿Estás listo para darle vida a tu marca? Si lo estás, cuentános más en el siguiente formulario de contacto</p>
+         </div>
+       </div>
+       <div class="row">
+         <div class="col-lg-12">
+           <form>
+             <div class="form-group">
+               <label for="InputEmail1">Correo electrónico</label>
+               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ingresa tu correo">
+               <small id="emailHelp" class="form-text text-muted">Nunca daremos a conocer tu datos con terceros</small>
+             </div>
+             <div class="form-group">
+               <label for="ControlTextarea1">Mensaje</label>
+               <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Escribe tu mensaje aquí"rows="3"></textarea>
+             </div>
+             <button type="submit" class="btn btn-primary btn-lg">Enviar</button>
+           </form>
+         </div>
+         <div class="col-lg-4 ml-auto text-center">
+           <i class="fa fa-phone fa-3x mb-3 sr-contact"></i>
+           <p>+56 2 123 456 73</p>
+         </div>
+         <div class="col-lg-4 mr-auto text-center">
+           <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+           <p>
+             <a href="mailto:your-email@your-domain.com">contacto@dmark.com</a>
+           </p>
+         </div>
+       </div>
+     </div>
+   </section>
+
+
+<?php get_footer() ?>
---
+++ header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset') ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php get_template_part('_includes/iOS', 'icons') ?>
+	<?php wp_head() ?>
+</head>
+<body id="page-top">
+	<!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="img/logo.png" alt=""></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">Acerca de</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Servicios</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#portfolio">Portafolio</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
